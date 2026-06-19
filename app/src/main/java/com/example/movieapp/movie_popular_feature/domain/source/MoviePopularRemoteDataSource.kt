b0cf1a3d9e17e04f0a86d523ca5d9364b387f885
Estudo Kotlin Android <mr6227017@gmail.com>
2026-06-19 18:36:09 -0300

    feat: cria a interface de contrato MoviePopularRemoteDataSource para dados remotos

---
+++ app/src/main/java/com/example/movieapp/movie_popular_feature/domain/source/MoviePopularRemoteDataSource.kt
@@ -0,0 +1,10 @@
+package com.example.movieapp.movie_popular_feature.domain.source
+
+import com.example.movieapp.core.data.remote.response.MovieResponse
+import com.example.movieapp.core.paging.MoviePagingSource
+
+interface MoviePopularRemoteDataSource {
+
+    fun getpopularMoviePagingSource(): MoviePagingSource
+    suspend fun getPopularMovie(page: Int): MovieResponse
+}
