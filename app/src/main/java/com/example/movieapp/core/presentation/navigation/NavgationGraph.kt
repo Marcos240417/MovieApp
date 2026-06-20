0f24ca04b0b361ead7194381029eab5b2cf6aecd
Marcos Rodrigues Barbosa terto <140373653+Marcos240417@users.noreply.github.com>
2026-06-20 14:39:51 -0300

    Clean up NavgationGraph.kt by removing comments
    Removed commented-out code and notes for clarity.

--- app/src/main/java/com/example/movieapp/core/presentation/navigation/NavgationGraph.kt
+++ app/src/main/java/com/example/movieapp/core/presentation/navigation/NavgationGraph.kt
@@ -21,12 +21,9 @@ fun NavigationGraph(
     NavHost(
         navController = navController,
         startDestination = MoviePopularDestination,
-        // ✅ SÊNIOR: Deixamos o modifier limpo aqui. Cada tela gerencia seus paddings de forma
-        // independente através de seus próprios Scaffolds, evitando margens duplas e bugs visuais.
         modifier = Modifier
     ) {
 
-        // --- 1. TELA DE FILMES POPULARES ---
         composable<MoviePopularDestination> {
             val viewModel: MoviePopularViewModel = koinViewModel()
             val pagingMovies = viewModel.moviesFlow.collectAsLazyPagingItems()
@@ -34,19 +31,14 @@ fun NavigationGraph(
             MoviePopularScreen(
                 movies = pagingMovies,
                 navigateToDetailMovie = { movieId ->
-                    // ✅ O Timber agora usa uma string limpa rastreando o ID do clique sem bugar o build!
                     Timber.tag("CLIQUE_FILME").d("Usuário clicou no filme com ID: $movieId")
-                    // Próximo passo: navController.navigate(MovieDetailDestination(id = movieId))
                 }
             )
         }
-
-        // --- 2. TELA DE PESQUISA ---
         composable<MovieSearchDestination> {
             Text(text = "Tela de Pesquisa")
         }
 
-        // --- 3. TELA DE FAVORITOS ---
         composable<MovieFavoriteDestination> {
             Text(text = "Tela de Favoritos")
         }
