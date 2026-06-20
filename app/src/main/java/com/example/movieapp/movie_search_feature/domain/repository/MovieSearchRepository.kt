c6fd205f7d9d443014b530e7053c936c465e4a74
Marcos Rodrigues Barbosa terto <140373653+Marcos240417@users.noreply.github.com>
2026-06-20 15:11:50 -0300

    Remove comment about action naming in repository
    Removed commented note regarding naming conventions for Paging.

--- app/src/main/java/com/example/movieapp/movie_search_feature/domain/repository/MovieSearchRepository.kt
+++ app/src/main/java/com/example/movieapp/movie_search_feature/domain/repository/MovieSearchRepository.kt
@@ -5,6 +5,5 @@ import com.example.movieapp.core.domain.model.MovieSearch
 import kotlinx.coroutines.flow.Flow
 
 interface MovieSearchRepository {
-    // ✅ Sênior: Nome correto da ação e agnóstico a classes de configuração do Paging
     fun getSearchMovies(query: String): Flow<PagingData<MovieSearch>>
 }
